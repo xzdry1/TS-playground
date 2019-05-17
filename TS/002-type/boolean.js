9134ad1206e1df5a483e707c1aeac571e3841fd4
xzdry1 <289331962@qq.com>
2019-05-17 12:46:41 +0800

    practice type

---
+++ TS/002-type/boolean.js
@@ -0,0 +1,14 @@
+// let isAdmin: boolean = true;
+// console.log(isAdmin);
+// let age: number = 0x1ff;
+// console.log(age);
+// let name1: string = 'hi';
+// console.log(name1);
+// let empty: void = undefined;
+// console.log(empty);
+var count = 1;
+function increment() {
+    count++;
+}
+increment();
+console.log(count);
---
+++ TS/002-type/boolean.ts
@@ -0,0 +1,19 @@
+// let isAdmin: boolean = true;
+// console.log(isAdmin);
+
+// let age: number = 0x1ff;
+// console.log(age);
+
+// let name1: string = 'hi';
+// console.log(name1);
+
+// let empty: void = undefined;
+// console.log(empty);
+let count: number = 1;
+
+
+function increment(): void {
+    count++;
+}
+increment();
+console.log(count);
