271005fd5048e63dce2a87780b5b1cadf35488ab
xzdry1 <289331962@qq.com>
2019-06-20 23:48:57 +0800

    todolist practise

---
+++ React/001_todolist/src/TodoList.js
@@ -0,0 +1,18 @@
+import React ,{Fragment}from 'react';
+
+function TodoList() {
+  return (
+    <Fragment>
+      <div>
+        <input></input>
+        <button>提交</button>
+      </div>
+      <ul>
+        <li>学英语</li>
+        <li>学英语</li>
+      </ul>
+    </Fragment>
+  )
+}
+
+export default TodoList;
