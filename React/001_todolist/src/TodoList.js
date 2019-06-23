ecbbb3ef78d4f6a89c34f50f7fc40c0069413eed
xzdry1 <289331962@qq.com>
2019-06-23 22:52:51 +0800

    use dangerouslySetInnerHTML

--- React/001_todolist/src/TodoList.js
+++ React/001_todolist/src/TodoList.js
@@ -1,56 +1,60 @@
 import React, { Component, Fragment } from 'react';
+import './style.css'
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            inputValue: '',
-            list: []
-        }
+    this.state = {
+      inputValue: '',
+      list: []
     }
+  }
 
-    render() {
-        return (
-            <Fragment>
-                <div>
-                    <input
-                        value={this.state.inputValue}
-                        onChange={this.handleInputChange.bind(this)}
-                    />
-                    <button
-                        onClick={this.handleSubmit.bind(this)}
-                    >submit</button>
-                </div>
-                <ul>
-                    {this.state.list.map((el,index)=>{
-                        return <li 
-                        key={index}
-                        onClick={this.handleDelete.bind(this,index)}>{el}</li>
-                    })}
-                </ul>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <input
+            className='input'
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button
+            onClick={this.handleSubmit.bind(this)}
+          >submit</button>
+        </div>
+        <ul>
+          {this.state.list.map((el, index) => {
+            return <li
+              key={index}
+              onClick={this.handleDelete.bind(this, index)}
+              dangerouslySetInnerHTML={{__html:el}}
+              ></li>
+          })}
+        </ul>
+      </Fragment>
+    )
+  }
 
-    handleInputChange(e) {
-        this.setState({
-            inputValue: e.target.value
-        })
-    }
-    handleSubmit() {
-        this.setState({
-            inputValue: '',
-            list: [...this.state.list,this.state.inputValue]
-        })
-    }
-    handleDelete(index){
-        let newList=[...this.state.list];
-        newList.splice(index,1);
-        this.setState({
-            list:newList
-        })
-    }
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleSubmit() {
+    this.setState({
+      inputValue: '',
+      list: [...this.state.list, this.state.inputValue]
+    })
+  }
+  handleDelete(index) {
+    let newList = [...this.state.list];
+    newList.splice(index, 1);
+    this.setState({
+      list: newList
+    })
+  }
 }
 
 export default TodoList;
