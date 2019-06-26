3cc4bf921cdc814a8f628618aa0d4a84d4665a79
xzdry1 <289331962@qq.com>
2019-06-27 00:06:54 +0800

    代码优化复习

--- React/001_todolist/src/TodoList1.js
+++ React/001_todolist/src/TodoList1.js
@@ -2,58 +2,68 @@ import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem1';
 
 class TodoList1 extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            inputValue: 'asd',
-            list: []
-        }
+    this.state = {
+      inputValue: '',
+      list: []
     }
-    render() {
-        return (
-            <Fragment>
-                <div>
-                    <input
-                        value={this.state.inputValue}
-                        onChange={this.handleInput.bind(this)} />
-                    <button
-                        onClick={this.handleSubmit.bind(this)}
-                    >submit</button>
-                </div>
-                <ul>
-                    {this.state.list.map((el, index) => {
-                        return (
-                            <TodoItem
-                            content={el}
-                            index={index}
-                            onClick={this.handleDelete}
-                            deleteItem={this.handleDelete.bind(this)}
-                            />
-                        )
-                    })}
-                </ul>
-            </Fragment>
-        )
-    }
-    handleInput(e) {
-        this.setState({
-            inputValue: e.target.value
-        })
-    }
-    handleDelete(index) {
-        let newList = [...this.state.list];
-        newList.splice(index, 1);
-        this.setState({
-            list: newList
-        })
-    }
-    handleSubmit() {
-        this.setState({
-            inputValue: '',
-            list: [...this.state.list, this.state.inputValue]
-        })
-    }
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInput} />
+          <button
+            onClick={this.handleSubmit}
+          >submit</button>
+        </div>
+        <ul>
+          {this.getTodoItem()}
+        </ul>
+      </Fragment>
+    )
+  }
+  getTodoItem() {
+    return this.state.list.map((el, index) => {
+      return (
+        <TodoItem
+          key={index}
+          content={el}
+          index={index}
+          deleteItem={this.handleDelete}
+        />
+      )
+    })
+  }
+  handleInput(e) {
+    const value = e.target.value;
+    this.setState(() => ({
+      inputValue: value
+    }))
+  }
+  handleDelete(index) {
+    this.setState((prevState) => {
+      const list = [...prevState.list];
+      list.splice(index, 1);
+      return {
+        list
+      }
+    })
+  }
+  handleSubmit() {
+    this.setState((prevState) => ({
+      inputValue: '',
+      list: [...prevState.list, prevState.inputValue]
+    }))
+  }
 }
 
 export default TodoList1;
--- React/001_todolist/src/index.js
+++ React/001_todolist/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
-// import TodoList from './TodoList1'
+// import TodoList from './TodoList';
+import TodoList from './TodoList1'
 // import TodoList from './TodoList2';
 
 
