915931996957b8958ac27fa72b7fe7b7706bf5a4
xzdry1 <289331962@qq.com>
2019-06-27 00:14:22 +0800

    方法的定义练习

---
+++ C#/008 方法的定义/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _008_方法的定义
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c=new Calculator();
+            System.Console.WriteLine(c.GetConeVolume(10,100));
+        }
+    }
+
+    class Calculator{
+        public double GetCircleArea(double r){
+            return Math.PI*r*r;
+        }
+
+        public double GetCylinderVolume(double r,double h){
+            return GetCircleArea(r)*h;
+        }
+
+        public double GetConeVolume(double r,double h){
+            return GetCylinderVolume(r,h)/3;
+        }
+    }
+}
