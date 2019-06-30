a54b8ded0a6ef618261000afdf00d0259728245b
xzdry1 <289331962@qq.com>
2019-06-30 23:40:36 +0800

    use antd ui

---
+++ React/001_todolist/src/TodoListWithRedux.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div style={{ marginTop: '10px', marginLeft: '10px' }}>
+        <div>
+          <Input placeholder="todo info" style={{ width: '200px' }} />
+          <Button type="dashed">Submit</Button>
+        </div>
+        <List
+          style={{marginTop:'10px',width:'270px'}}
+          bordered
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+
+    )
+  }
+}
+
+export default TodoList;
--- React/001_todolist/src/index.js
+++ React/001_todolist/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import TodoList from './TodoList';
 // import TodoList from './TodoList1'
-import TodoList from './TodoList2';
+// import TodoList from './TodoList2';
+import TodoList from './TodoListWithRedux'
 
 
 ReactDOM.render(<TodoList />, document.getElementById('root'));
