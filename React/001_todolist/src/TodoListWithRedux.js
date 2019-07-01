2c499f98914dfd7093bca56d5307e2c73f85a513
xzdry1 <289331962@qq.com>
2019-07-01 23:38:02 +0800

    learning redux flow

--- React/001_todolist/src/TodoListWithRedux.js
+++ React/001_todolist/src/TodoListWithRedux.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+import store from './store'
 
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = store.getState();
+
+  }
+
   render() {
     return (
       <div style={{ marginTop: '10px', marginLeft: '10px' }}>
         <div>
-          <Input placeholder="todo info" style={{ width: '200px' }} />
+          <Input value={this.state.inputVal} placeholder="todo info" style={{ width: '200px' }} />
           <Button type="dashed">Submit</Button>
         </div>
         <List
-          style={{marginTop:'10px',width:'270px'}}
+          style={{ marginTop: '10px', width: '270px' }}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (
             <List.Item>
               {item}
---
+++ React/001_todolist/src/store/reducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+  inputVal: '123',
+  list: [1,2]
+}
+
+
+export default (state = defaultState, action) => {
+  return state;
+}
